e421c4ad7d0d831fdd863d4c5f013a51b6acd4df
AleksandrL0579 <a.lelsan@ukr.net>
2023-10-14 16:49:11 +0300

    js-auth-1

---
+++ src/route/auth.js
@@ -0,0 +1,78 @@
+// Підключаємо технологію express для back-end сервера
+const express = require('express')
+// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
+const router = express.Router()
+
+const { User } = require('../class/user')
+
+// ================================================================
+
+router.get('/signup', function (req, res) {
+  res.render('signup', {
+    name: 'signup',
+
+    component: [
+      'back-button',
+      'field',
+      'field-password',
+      'field-checkbox',
+      'field-select',
+    ],
+
+    title: 'Signup page ',
+
+    data: {
+      role: [
+        { value: User.USER_ROLE.USER, text: 'Користувач' },
+        {
+          value: User.USER_ROLE.ADMIN,
+          text: 'Адміністратор',
+        },
+        {
+          value: User.USER_ROLE.DEVELOPER,
+          text: 'Розробник',
+        },
+      ],
+    },
+  })
+})
+
+router.post('/signup', function (req, res) {
+  const { email, password, role } = req.body
+
+  if (!email || !password || !role) {
+    return res.status(400).json({
+      message: "Помилка. Обов'язкові поля відсутні",
+    })
+  }
+
+  try {
+    const user = User.getByEmail(email)
+
+    if (user) {
+      return res.status(401).json({
+        message: 'Користувач з таким e-mail вже уснує',
+      })
+    }
+
+    const newUser = User.create({ email, password, role })
+
+    const session = Session.create(newUser)
+
+    Confirm.create(newUser.email)
+
+    return res.status(200).json({
+      message: 'Користувач успішно зареєстрований',
+      session,
+    })
+  } catch (error) {
+    return res.status(400).json({
+      // message: error.message,
+      message: 'Помилка створення користувача',
+    })
+  }
+})
+
+// ================================================================
+
+module.exports = router
--- src/route/index.js
+++ src/route/index.js
@@ -1,13 +1,55 @@
-// Підключаємо роутер до бек-енду
 const express = require('express')
 const router = express.Router()
 
-// Підключіть файли роутів
-const test = require('./test')
-// Підключіть інші файли роутів, якщо є
+router.get('/', function (req, res) {
+  res.render('index', {
+    name: 'index',
 
+    component: [
+      'back-button',
+      'field',
+      'field-password',
+      'field-checkbox',
+      'field-select',
+    ],
+
+    title: 'Index Page',
+
+    data: {},
+  })
+})
+
+router.get('/home', function (req, res) {
+  res.render('home', {
+    name: 'home',
+
+    component: ['back-button', 'field'],
+
+    title: 'Home Page',
+
+    data: {},
+  })
+})
+
+router.get('/logout', function (req, res) {
+  res.render('logout', {
+    name: 'logout',
+
+    component: [],
+
+    title: 'Logout Page',
+
+    data: {},
+  })
+})
+
+const auth = require('./auth')
+
+// Підключіть інші файли роутів, якщо є
 // Об'єднайте файли роутів за потреби
-router.use('/', test)
+
+router.use('/', auth)
+
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
